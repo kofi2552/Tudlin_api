3ad638d2dddeb67ee5980b21a0da0f93aa21600f
kofi2552 <kofiloyke2552@gmail.com>
2025-02-28 19:26:15 +0000

    fixed port binding

--- server.js
+++ server.js
@@ -31,7 +31,7 @@ import uploadFileRoutes from "./routes/uploadFileRoutes.js";
 const app = express();
 
 dotenv.config();
-const port = 5000;
+const port = process.env.PORT || 5000;
 
 const server = http.createServer(app);
 
