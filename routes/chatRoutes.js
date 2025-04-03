3c069eecaa7c0cb81a10b4717dd8184cc01fbc9d
kofi2552 <kofiloyke2552@gmail.com>
2025-04-03 15:48:47 +0000

    Fix Chat Create routes

--- routes/chatRoutes.js
+++ routes/chatRoutes.js
@@ -12,20 +12,20 @@ import { verifyToken } from "../middleware/jwt.js";
 
 const router = express.Router();
 
-//router.post("/create/:schoolId", CreateChat);
-// router.post("/create-group/:schoolId", CreateGroupChat);
-// router.post("/send-message", sendMessage);
-// router.get("/messages/:chatId", getMessages);
-// router.get("/all/:schoolId", fetchAllChats);
-// router.delete("/del/:chatId", deleteChat);
-// router.put("/edit/:chatId", updateChat);
-
-router.post("/create/:schoolId", verifyToken, CreateChat);
-router.post("/create-group/:schoolId", verifyToken, CreateGroupChat);
-router.post("/send-message", verifyToken, sendMessage);
+router.post("/create/:schoolId", CreateChat);
+router.post("/create-group/:schoolId", CreateGroupChat);
+router.post("/send-message", sendMessage);
 router.get("/messages/:chatId", getMessages);
-router.get("/all/:schoolId", verifyToken, fetchAllChats);
-router.delete("/del/:chatId", verifyToken, deleteChat);
-router.put("/edit/:chatId", verifyToken, updateChat);
+router.get("/all/:schoolId", fetchAllChats);
+router.delete("/del/:chatId", deleteChat);
+router.put("/edit/:chatId", updateChat);
+
+// router.post("/create/:schoolId", verifyToken, CreateChat);
+// router.post("/create-group/:schoolId", verifyToken, CreateGroupChat);
+// router.post("/send-message", verifyToken, sendMessage);
+// router.get("/messages/:chatId", getMessages);
+// router.get("/all/:schoolId", verifyToken, fetchAllChats);
+// router.delete("/del/:chatId", verifyToken, deleteChat);
+// router.put("/edit/:chatId", verifyToken, updateChat);
 
 export default router;
